906b920167895abb08f323cf0f3888b52341b54e
Manikanta2 <manikanta1596@gmail.com>
2019-07-16 18:08:02 -0700

    Added API's to add, delete and modify a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -37,4 +37,58 @@ router.post('/', [auth, [
     }
 });
 
+
+
+// @route     GET api/posts
+// @desc      Get posts
+// @access    Private
+router.get('/', auth, async (req, res) => {
+   try{
+       const posts = await Post.find().sort({date: -1});
+       return res.json(posts);
+   } catch(err){
+       console.error(err.message);
+       res.status(500).send('Server Error');
+   }
+});
+
+
+// @route     GET api/posts/:post_id
+// @desc      Get post by Id
+// @access    Private
+router.get('/:post_id', auth, async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.post_id);
+        if(!post){
+            return res.status(404).send('Post not found');
+        }
+        return res.json(post);
+    } catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+// @route     DELETE api/posts/:post_id
+// @desc      Delete posts by it's Id
+// @access    Private
+router.delete('/post/:post_id', auth, async (req, res) => {
+   try{
+       const post = await Post.findById(req.params.post_id);
+
+       // Check User
+       if(post.user.toString() !== req.user.id){
+           return res.status(401).json({msg: 'User not authorized'});
+       }
+
+       await post.remove();
+
+       return res.send('Post removed');
+   } catch(err){
+       console.error(err.message);
+       res.status(500).send('Server Error');
+   }
+});
+
 module.exports = router;
