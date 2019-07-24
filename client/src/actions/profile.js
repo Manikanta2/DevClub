98b8ac9415ea692ff3f294079ca875d3d57a3d78
Manikanta2 <manikanta1596@gmail.com>
2019-07-23 21:06:05 -0700

    Added reducer for profile and getCurrentUserProfile action to retrieve current user's profile object

---
+++ client/src/actions/profile.js
@@ -0,0 +1,20 @@
+import {PROFILE_ERROR, GET_PROFILE} from "./types";
+import axios from 'axios';
+
+
+// Get current user's profile
+export const getCurrentProfile = () => async dispatch => {
+    try{
+        const res = await axios.get('/api/profile/me');
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        });
+    }
+};
