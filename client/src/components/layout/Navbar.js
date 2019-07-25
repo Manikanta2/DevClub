09e845123370115592f4531b5bbafa21cfa917b3
Manikanta2 <manikanta1596@gmail.com>
2019-07-24 19:11:51 -0700

    Started with dashboard and added Spinner component to display loading

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,8 +7,11 @@ import PropTypes from 'prop-types';
 const Navbar = ({logout, auth: {isAuthenticated, loading}}) => {
     const authLinks = (
         <ul>
+            <li><Link to="/dashboard">
+                <i className='fas fa-user' />{' '}
+                <span className='hide-sm'>Dashboard</span></Link></li>
             <li>
-                <a onClick={logout} href='!#'><i className="fas fa-sign-out-alt"></i>{' '} <span className="hide-sm">Logout</span></a>
+                <a onClick={logout} href='!#'><i className="fas fa-sign-out-alt"/>{' '} <span className="hide-sm">Logout</span></a>
 
             </li>
         </ul>
@@ -24,7 +27,7 @@ const Navbar = ({logout, auth: {isAuthenticated, loading}}) => {
     return (
         <nav className="navbar bg-dark">
             <h1>
-                <Link to="/"><i className="fas fa-code"></i> DevConnector</Link>
+                <Link to="/"><i className="fas fa-code"/> DevConnector</Link>
             </h1>
             {!loading && (<Fragment>{isAuthenticated? authLinks: guestLinks}</Fragment>)}
         </nav>
