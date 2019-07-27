7622cc4f874dade7ddcf750056d9781c61124537
Manikanta2 <manikanta1596@gmail.com>
2019-07-27 16:28:45 -0700

    Finishing profiles action with get all profiles and get top five git repos

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,4 +1,4 @@
-import {PROFILE_ERROR, GET_PROFILE, UPDATE_PROFILE, CLEAR_PROFILE, DELETE_ACCOUNT} from "./types";
+import {PROFILE_ERROR, GET_PROFILE, UPDATE_PROFILE, CLEAR_PROFILE, DELETE_ACCOUNT, GET_PROFILES, GET_REPOS} from "./types";
 import axios from 'axios';
 import {setAlert} from "./alert";
 
@@ -183,5 +183,58 @@ export const deleteAccount = () => async dispatch => {
                     payload: {msg: err.response.statusText, status: err.response.status}
                 });
             }
+    }
+};
+
+// Get all the profiles
+export const getProfiles = () => async dispatch => {
+    dispatch({type: CLEAR_PROFILE});
+    try{
+        const res = await axios.get('/api/profile');
+
+        dispatch({
+            type: GET_PROFILES,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        });
+    }
+};
+
+
+// Get profiles by Id
+export const getProfileById = userId => async dispatch => {
+    try{
+        const res = await axios.get(`/api/profile/user/${userId}`);
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        });
+    }
+};
+
+// Get Git repo's
+export const getGitHubRepos = username => async dispatch => {
+    try{
+        const res = await axios.get(`/api/profile/github/${username}`);
+
+        dispatch({
+            type: GET_REPOS,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        });
     }
 };
