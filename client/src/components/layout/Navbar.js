a1ad02b2025bfb774b0555192b95c3d8523649a0
Manikanta2 <manikanta1596@gmail.com>
2019-07-28 19:31:39 -0700

    Added Post action, reducer and Posts component to get all posts

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -11,6 +11,7 @@ const Navbar = ({logout, auth: {isAuthenticated, loading}}) => {
             <li><Link to="/dashboard">
                 <i className='fas fa-user' />{' '}
                 <span className='hide-sm'>Dashboard</span></Link></li>
+            <li><Link to='/posts'>Posts</Link></li>
             <li>
                 <a onClick={logout} href='!#'><i className="fas fa-sign-out-alt"/>{' '} <span className="hide-sm">Logout</span></a>
 
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -12,4 +12,23 @@ const initialState = {
     error: {}
 };
 
-export default post;
+export default function(state= initialState, action) {
+    const {type, payload} = action;
+
+    switch(type){
+        case GET_POSTS:
+            return {
+                ...state,
+                posts: payload,
+                loading: false
+            };
+        case POST_ERROR:
+            return {
+              ...state,
+              error: payload,
+              loading: false
+            };
+        default:
+            return{ ...state};
+    }
+}
