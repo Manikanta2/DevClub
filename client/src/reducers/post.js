03cfd0ac9d4b6749466e768e7609d376214a4005
Manikanta2 <manikanta1596@gmail.com>
2019-07-28 19:08:02 -0700

    Added Profile Experience, Education and Github Repos to the view profile component

---
+++ client/src/reducers/post.js
@@ -0,0 +1,15 @@
+import {
+    GET_POSTS,
+    POST_ERROR
+} from '../actions/types';
+
+
+
+const initialState = {
+    posts: [],
+    post: null,
+    loading: true,
+    error: {}
+};
+
+export default post;
